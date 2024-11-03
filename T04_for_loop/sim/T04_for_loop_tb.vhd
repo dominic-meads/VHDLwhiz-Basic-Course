45ac3175f906b1cb2f62c60d5668295003ba3062
dominic-meads <de.meads10@gmail.com>
2024-11-02 17:25:31 -0700

    add comment about for loop step parameter

--- T04_for_loop/sim/T04_for_loop_tb.vhd
+++ T04_for_loop/sim/T04_for_loop_tb.vhd
@@ -7,7 +7,7 @@ entity T04_ForLoopTb is
       process is
       begin
     
-          for i in 1 to 10 loop
+          for i in 1 to 10 loop  -- for loop has no step parameter, only increments 1 or decrements by 1
             report "i=" & integer'image(i);  -- to print in VHDL, must be string, integer'image(i) converts i to a string
           end loop;
           wait;
