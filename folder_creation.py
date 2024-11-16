b35d5589d79523cb74d3935f747c06540551816f
dominic-meads <de.meads10@gmail.com>
2024-11-15 16:41:03 -0800

    update

--- folder_creation.py
+++ folder_creation.py
@@ -1,7 +1,10 @@
 import os
 
 for x in range(5, 24):  # already made first 4 folders
-  folder_name = "T0" + str(x)
+  if x <= 9:
+    folder_name = "T0" + str(x)
+  else: 
+    folder_name = "T" + str(x)
   subfolder_names = ["src", "sim"]
   for subfolder_name in subfolder_names:
       os.makedirs(os.path.join(folder_name, subfolder_name))
